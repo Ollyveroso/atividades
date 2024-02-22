810fe1d6c0d7ed16ef3a912043e180cf8616d240
Ollyveroso <126161749+Ollyveroso@users.noreply.github.com>
2024-02-22 09:59:25 -0300

    Create S1-02C app.js

---
+++ S1-02C app.js
@@ -0,0 +1,40 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+        <Text style={styles.abc}>ollyver ośorio, armario 2, numero 11</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  titulo: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    marginTop: 20,
+    color: 'white',
+  },
+  abc: {
+    backgroundColor: 'gray',
+    borderWidth: 1,
+    borderColor: 'black',
+    padding: 10,
+    textAlign: 'center',
+    fontSize: 16,
+    color: 'white',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'flex-start', // Alinha os elementos à esquerda
+    paddingTop: 50,
+    paddingLeft: 20, // Adiciona um padding à esquerda para o primeiro elemento Text
+  },
+  aa: {
+    marginTop: 20, // Adiciona um espaço entre o primeiro e o segundo elemento Text
+  },
+});
